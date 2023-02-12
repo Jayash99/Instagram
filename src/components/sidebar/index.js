00f7630334c2ccf27131790b8dd20ef225c04b17
Jayash99 <jayashgarg99@gmail.com>
2023-02-12 17:15:31 +0530

    Bug Fix

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,12 +1,11 @@
+/* eslint-disable prettier/prettier */
 import { useContext } from 'react';
 import User from './user';
 import Suggestions from './suggestions';
 import LoggedInUserContext from '../../context/logged-in-user';
 
 export default function Sidebar() {
-  const { user: { docId = '', fullName, username, userId, following } = {} } = useContext(
-    LoggedInUserContext
-  );
+  const { user: { docId = '', fullName, username, userId, following } = {} } = useContext(LoggedInUserContext);
 
   return (
     <div className="p-4">
